af4b92f81264795b349334332889b40d5a6c6e63
Shadowdragon8969 <shadowdragon8969@gmail.com>
2018-01-11 13:49:28 -0600

    Create EntityLiving.cs

---
+++ GameWorld/EntityLiving.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameWorld
+{
+    public class EntityLiving : Entity
+    {
+
+        public double health = 0;
+        
+
+        public EntityLiving(String name) : base(name)
+        {
+
+        }
+
+    }
+}
